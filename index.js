a1119a76a80c8747e6b9a743f1e4d834f152b105
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2021-10-16 19:55:30 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const nextBtn = document.querySelector('#nextBtn');
 
 //counter
 let counter = 1;
-const size = carouselImages[0].clientWidth;
+const size = 700;
 
 carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
